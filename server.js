555fc23723aa7c869640ccd0644dd833b9b052b8
mac <mac@macs-Mac-mini.local>
2021-09-21 18:47:32 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const port = 3000;
 
 
 
-app.get('/greeting/:name', (req, res) => {
-  res.send("Hello, stranger");
+app.get('/greeting/:firstName', (req, res) => {
+    res.send(`Hello, ${req.params.firstName}`);
+});
+
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Tip Amount: ${req.params.total / req.params.tipPercentage }`);
 });
 
 
 
-app.listen(port,() => {
-    console.log('listening on port' , port);
+
+
+app.listen(port, () => {
+    console.log('listening on port', port);
 });
