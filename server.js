86cb8234d93b66d4f1fd4252a5c40f3b4bb6e2e6
mac <mac@macs-Mac-mini.local>
2021-09-21 20:01:08 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const magic8 = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
 
 
 app.get('/greeting/:firstName', (req, res) => {
@@ -10,9 +12,16 @@ app.get('/greeting/:firstName', (req, res) => {
 
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
-    res.send(`Tip Amount: ${req.params.total / req.params.tipPercentage }`);
+    res.send(`Tip Amount: ${req.params.total * req.params.tipPercentage / 100 }`);
 });
 
+
+
+app.get('/magic/:quastion', (req, res) => {
+    let response = magic8[Math.floor(Math.random() * magic8.length)];;
+    res.send(`<h1>${response}</h1>`);
+
+});
 
 
 
